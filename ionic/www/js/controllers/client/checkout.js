273472a4c0e53e04f234ab5b03e7870788cf91e2
vagrant <vagrant@homestead>
2018-05-21 20:14:32 -0700

    Adicionado funcionalidade para inserir o cupom no pedido

--- ionic/www/js/controllers/client/checkout.js
+++ ionic/www/js/controllers/client/checkout.js
@@ -1,15 +1,16 @@
 angular.module('starter.controllers')
     .controller('ClientCheckoutCtrl', [
-        '$scope', '$state', '$cart', 'Order', '$ionicLoading','$ionicPopup',
-        function ($scope, $state, $cart, Order, $ionicLoading,$ionicPopup) {
+        '$scope', '$state', '$cart', 'Order', '$ionicLoading','$ionicPopup', 'Cupom',
+        function ($scope, $state, $cart, Order, $ionicLoading,$ionicPopup, Cupom) {
             var cart = $cart.get();
+            $scope.cupom = cart.cupom;
             $scope.items = cart.items;
-            $scope.total = cart.total;
+            $scope.total = $cart.getTotalFinal();
 
             $scope.removeItem = function (i) {
                 $cart.removeItem(i);
                 $scope.items.splice(i, 1);
-                $scope.total = $cart.get().total;
+                $scope.total = $cart.getTotalFinal();
             };
 
             $scope.openListProducts = function () {
@@ -21,24 +22,57 @@ angular.module('starter.controllers')
             };
             
             $scope.save = function () {
-                var items = angular.copy($scope.items);
+                var o = {items: angular.copy($scope.items)};
                 
-                angular.forEach(items, function (item) {
+                angular.forEach(o.items, function (item) {
                     item.product_id = item.id;
                 });
                 $ionicLoading.show({
                     template: 'Carregando...'
                 });
-                
-                Order.save({id:null},{items:items}, function (data){
+
+                if($scope.cupom.value){
+                    o.cupom_code = $scope.cupom.code;
+                }
+                Order.save({id:null}, o, function (data){
                     $ionicLoading.hide();
                     $state.go('client.checkout_successful');
                 },function(responseError) {
+                    console.log(responseError);
                     $ionicLoading.hide();
                     $ionicPopup.alert({
                         title: 'Advertência',
                         template: 'Pedido não realizado. Tente novamente.'
                     });
                 });
-            }
+            };
+
+            $scope.readBarCode = function () {
+                getValueCupom(9320);
+            };
+
+            $scope.removeCupom = function () {
+                $cart.removeCupom();
+                $scope.cupom = $cart.get().cupom;
+                $scope.total = $cart.getTotalFinal();
+            };
+
+            function getValueCupom(code) {
+                $ionicLoading.show({
+                    template: 'Carregando...'
+                });
+                Cupom.get({code: code}, function (data) {
+                    $cart.setCupom(data.data.code, data.data.value);
+                    $scope.cupom = $cart.get().cupom;
+                    $scope.total = $cart.getTotalFinal();
+                    $ionicLoading.hide();
+                },function (responseError) {
+                    $ionicLoading.hide();
+                    $ionicPopup.alert({
+                        title: 'Advertência',
+                        template: 'Cupom inválido!'
+                    });
+                });
+            };
+
         }]);
